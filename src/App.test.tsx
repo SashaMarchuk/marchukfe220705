b7c2268055943f5588e07262f6f55a3dee36c619
Sasha Marchuk <Sasha.Marchuk.email@gmail.com>
2022-07-05 22:49:37 +0300

    Add tests to app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,89 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { fireEvent, render, screen } from "@testing-library/react";
+import App, { ICharts } from "./App";
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test("renders text: Spend Time (Seconds)", () => {
+  render(<App />);
+  const linkElement = screen.getByText("Spend Time (Seconds)");
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders text: LandingPage", () => {
+  render(<App />);
+  const linkElement = screen.getByText("LandingPage");
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders text: Configurator", () => {
+  render(<App />);
+  const linkElement = screen.getByText("Configurator");
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders text: Check-out", () => {
+  render(<App />);
+  const linkElement = screen.getByText("Check-out");
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders text: Deal", () => {
+  render(<App />);
+  const linkElement = screen.getByText("Deal");
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders button shuffle", () => {
+  render(<App />);
+  const button = screen.getByRole("button", { name: "Shuffle" });
+  expect(button).toBeInTheDocument();
+});
+
+test("renders button random", () => {
+  render(<App />);
+  const button = screen.getByRole("button", { name: "Random value" });
+  expect(button).toBeInTheDocument();
+});
+
+test("click shuffle", () => {
+  render(<App />);
+  fireEvent.click(screen.getByText("Shuffle"));
+
+  const charts: ICharts[] = [
+    { name: "LandingPage", time: 7.4 },
+    { name: "Configurator", time: 0.2 },
+    { name: "Check-out", time: 7.0 },
+    { name: "Deal", time: 3.8 },
+  ];
+
+  charts.forEach((item) => {
+    const { name, time } = item;
+
+    const textNameElement = screen.getByText(name);
+    const textTimeElement = screen.getByText(time);
+
+    expect(textNameElement).toBeInTheDocument();
+    expect(textTimeElement).toBeInTheDocument();
+  });
+});
+
+test("click random", () => {
+  render(<App />);
+  fireEvent.click(screen.getByText("Random value"));
+
+  const charts: ICharts[] = [
+    { name: "LandingPage", time: 7.4 },
+    { name: "Configurator", time: 0.2 },
+    { name: "Check-out", time: 7.0 },
+    { name: "Deal", time: 3.8 },
+  ];
+
+  charts.forEach((item) => {
+    const { name, time } = item;
+
+    const textNameElement = screen.getByText(name);
+    const textTimeElement = screen.queryByText(time);
+
+    expect(textNameElement).toBeInTheDocument();
+    expect(textTimeElement).toBeNull();
+  });
+});
--- src/components/Chart/Chart.test.tsx
+++ src/components/Chart/Chart.test.tsx
@@ -2,13 +2,24 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Chart from "./Chart";
+import { ICharts } from "../../App";
 
-// describe('<Chart />', () => {
-//   test('it should mount', () => {
-//     render(<Chart />);
+test("Render chart name && time", () => {
+  const charts: ICharts[] = [
+    { name: "LandingPage", time: 7.4 },
+    { name: "Configurator", time: 0.2 },
+    { name: "Check-out", time: 7.0 },
+    { name: "Deal", time: 3.8 },
+  ];
 
-//     const chart = screen.getByTestId('Chart');
+  charts.forEach((item) => {
+    const { time, name } = item;
+    render(<Chart time={time} name={name} />);
 
-//     expect(chart).toBeInTheDocument();
-//   });
-// });
+    const isName = screen.getByText(name);
+    const isTime = screen.getByText(time);
+
+    expect(isName).toBeInTheDocument();
+    expect(isTime).toBeInTheDocument();
+  });
+});
